3ebf90f53c2296b8703a3057d02c4b4e50b0ce5a
Christian Zöller <No3x@users.noreply.github.com>
2024-05-13 20:13:15 +0200

    Add body limit

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const osmtogeojson = require('osmtogeojson');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json({limit: process.env.BODYLIMIT || '50mb'}));
 
 app.post('/convert', (req, res) => {
-    const data  = req.body;
+    const data = req.body;
     try {
         // Convert OSM data to GeoJSON
         const geoJson = osmtogeojson(data);
         res.json(geoJson);
     } catch (error) {
-        res.status(500).json({ error: 'An error occurred while converting OSM to GeoJSON' });
+        res.status(500).json({error: 'An error occurred while converting OSM to GeoJSON'});
     }
 });
 
